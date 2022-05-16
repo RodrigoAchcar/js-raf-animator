ed5486d60920c983a0f939a542f107530f5ec11a
RodrigoAchcar <71414442+RodrigoAchcar@users.noreply.github.com>
2022-05-15 23:48:38 -0300

    Update js-raf-animator.js

--- js-raf-animator.js
+++ js-raf-animator.js
@@ -1,45 +1,53 @@
-function startAnimating(elementName, styleI, styleO, startV, endV , seconds, fps) {	
-		let element = document.getElementById(elementName);
-		let fpsD, currentT, previousT, increment, iterate, fpsCounter;	
-		fpsD = Math.floor(1000 / fps);
-		totalFrames = (seconds * 1000) / fpsD; //miliseconds duration
-		previousT = 0;
-		currentT = previousT;		
-		deltaV = endV - startV;
-		fpsCounter = 0; // 4 log
-		render(element, styleI, styleO, fpsD, currentT, previousT, startV, endV, fpsCounter);
+function startAnimating(element, seconds, fps, styleI, styleO, startV, endV) {		
+	let fpsD = Math.floor(1000 / fps);
+	let totalFrames = (seconds * 1000) / fpsD; //miliseconds duration
+	let previousT = 0;
+	let currentT = 0;
+	let fpsCounter = 0;		
+
+	let deltaV = endV - startV;
+	console.log(performance.now());
+	render(element, seconds, fps, fpsD, styleI, styleO, startV, endV, deltaV, currentT, previousT, totalFrames, fpsCounter);
+}
+function render(element, seconds, fps, fpsD, styleI, styleO, startV, endV, deltaV, currentT, previousT, totalFrames, fpsCounter) {	
+	currentT = currentT + fps;		
+	let fpsCount = currentT - previousT;
+	if (fpsCount >= fpsD) {			
+		if(fpsCounter <= totalFrames) {						
+			let ease = easeInOutQuad( fpsCounter, startV, deltaV, totalFrames);
+			// animate element code here					
+			element.style.transform = 'translateX(' + ease + 'px)';	
+			fpsCounter++;		
+			previousT = currentT;
+		} else {		
+			element.style.transform = 'translateX(' + endV + 'px)';	
+			cancelAnimationFrame(render);
+			console.log('fpsCounter: ', fpsCounter);
+			console.log(performance.now());
+			return;
+		}				
 	}
-	function render(element, styleI, styleO, fpsD, currentT, previousT, startV, endV, fpsCounter) {	
-		currentT = currentT + (fpsD * 1000);		
-		fpsCount = currentT - previousT;
-		if (fpsCount >= fpsD) {			
-			if(fpsCounter <= totalFrames) {						
-				let ease = easeInOutQuad( fpsCounter, startV, deltaV, totalFrames);		
-				element.style.transform = styleI + ease + styleO;	
-				fpsCounter++;		
-				previousT = currentT;
-			} else {		
-				element.style.transform = styleI + endV + styleO;	
-				cancelAnimationFrame(render);
-				return;
-			}				
-		}
-		requestAnimationFrame(function() {
-			render(element, styleI, styleO, fpsD, currentT, previousT, startV, endV, fpsCounter);
-		});			
-	}		
-	function easeInOutQuad(t, b, c, d) {
-		if ((t /= d / 2) < 1) return c / 2 * t * t + b;
-		return -c / 2 * ((--t) * (t - 2) - 1) + b;
-	}
-  // Custom function to animate an element by id
-	function runCarousel(seconds){
-		let imgsArray = document.getElementsByClassName('carousel_image');		
-		let startV = imgsArray[0].offsetLeft;
-		let endV = -imgsArray[0].getBoundingClientRect().width; 
-		let fps = 60;
-		let styleI = 'translateX(';
-		let styleO = 'px)';
-		let goBanner = startAnimating('carouselChain', styleI, styleO, startV, endV, seconds, fps);
-	}	
-	runCarousel(1);
+	requestAnimationFrame(function(){
+		render(element, seconds, fps, fpsD, styleI, styleO, startV, endV, deltaV, currentT, previousT, totalFrames, fpsCounter);
+	});	
+}	
+function easeInOutQuad(t, b, c, d) {
+	if ((t /= d / 2) < 1) return c / 2 * t * t + b;
+	return -c / 2 * ((--t) * (t - 2) - 1) + b;
+}
+
+function runCarousel(){
+	let element = document.getElementById('carouselChain');
+	let vector = 1;
+	let imgsArray = document.getElementsByClassName('carousel_image');		
+	let startV = element.offsetLeft;
+	let endV = -imgsArray[0].getBoundingClientRect().width; 		
+	let fps = 60;
+	let seconds = 2;
+	let styleI = 'translateX(';
+	let styleO = 'px)';
+
+	let goBanner = startAnimating(element, seconds, fps, styleI, styleO, startV, endV);		
+}	
+runCarousel();
+
